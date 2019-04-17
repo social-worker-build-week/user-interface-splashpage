326637fdf3bf493c93b0fece4362ccc42fa527f8
Leonel <leonelaf12@gmail.com>
2019-04-17 03:17:57 -0400

    finished the menu dropdown button with js

--- menu.js
+++ menu.js
@@ -1,2 +1,14 @@
+const toggleMenu = () => {
+    menu.classList.toggle('menu--open');
+}
+
 const menu = document.querySelector('.menu');
 console.log(menu);
+
+const menuButton = document.querySelector(".menu-button")
+console.log(menuButton);
+
+menuButton.addEventListener('click', () =>{
+    toggleMenu();
+});
+
